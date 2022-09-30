41f498b36abd53461b541e6fc425394212ce4017
Shoxbozbek <shoxbozbek9798@gmail.com>
2022-09-30 15:09:35 +0300

    main

---
+++ furn/translate.py
@@ -0,0 +1,7 @@
+# from dataclasses import fields
+# from modeltranslation.translator import register, TranslationOptions
+# from .models import TranslatePage
+
+# @register(TranslatePage)
+# class MalumotTranslateOption(TranslationOptions):
+#     fields = ('name', 'title', )
